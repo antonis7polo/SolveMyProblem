26bfbfab6452f2bee11117766a399f45159356a0
ntua-el20167 <el20167@mail.ntua.gr>
2024-04-30 16:12:03 +0300

    Signup MS

---
+++ signup-service/src/app.js
@@ -0,0 +1,20 @@
+require('dotenv').config({ path: '../.env' });
+const express = require('express');
+const connectDB = require('./config/database');
+
+const app = express();
+connectDB();  
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello, your server is running!');
+});
+
+const userRoutes = require('./routes/userRoutes');
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ signup-service/src/controllers/userController.js
@@ -0,0 +1,19 @@
+
+const User = require('../models/User');
+const { publishUserCreated } = require('../config/rabbitMQ');
+
+exports.createUser = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const newUser = new User({ username, email, password });
+        await newUser.save();
+
+        
+        await publishUserCreated({ username, email });
+
+        res.status(201).json({ message: 'User created successfully', user: newUser });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error creating user', error: error.message });
+    }
+};
