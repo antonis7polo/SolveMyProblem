bebfd3396b72a6105dda190be0b369b53049ae12
ntua-el20158 <el20158@mail.ntua.gr>
2024-06-02 21:15:44 +0300

    logs sorted (latest logs first)

--- log-management-service/controllers/getLogsController.js
+++ log-management-service/controllers/getLogsController.js
@@ -21,7 +21,7 @@ async function getLogs(req, res) {
     }
 
     try {
-        const logs = await Logs.find(filter);
+        const logs = await Logs.find(filter).sort({ executionTimestamp: -1 });;
         res.status(200).json(logs);
     } catch (error) {
         res.status(500).json({ error: error.message });
