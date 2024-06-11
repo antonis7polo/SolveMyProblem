4fee125004878e8e21a08a2bad764ecc21da9e01
ntua-el20167 <el20167@mail.ntua.gr>
2024-06-11 20:34:55 +0300

    Docker final v1

--- results-management-service/config/rabbitMQ.js
+++ results-management-service/config/rabbitMQ.js
@@ -10,31 +10,37 @@ const submissionExchange = process.env.SUBMISSION_EXCHANGE;
 const logExchange = process.env.LOG_EXCHANGE;
 
 async function setupRabbitMQ() {
-    const connection = await amqp.connect(process.env.RABBITMQ_URL);
-    channel = await connection.createChannel();
+    const RABBITMQ_URL = process.env.RABBITMQ_URL;
+    try {
+        const connection = await amqp.connect(RABBITMQ_URL);
+        channel = await connection.createChannel();
+        console.log('RabbitMQ correctly connected');
 
-    // Configuring exchanges
-    await channel.assertExchange(solverExchange, 'direct', { durable: true });
-    await channel.assertExchange(submissionExchange, 'direct', { durable: true });
-    await channel.assertExchange(logExchange, 'direct', { durable: true });
-    await channel.assertExchange(process.env.RESULT_DELETE_EXCHANGE_NAME, 'direct', { durable: true });
+        await channel.assertExchange(solverExchange, 'direct', { durable: true });
+        await channel.assertExchange(submissionExchange, 'direct', { durable: true });
+        await channel.assertExchange(logExchange, 'direct', { durable: true });
+        await channel.assertExchange(process.env.RESULT_DELETE_EXCHANGE_NAME, 'direct', { durable: true });
 
     // Configuring queues
-    await channel.assertQueue(process.env.SOLVER_QUEUE , { durable: true });
-    await channel.assertQueue(process.env.LOG_QUEUE, { durable: true });
-    await channel.assertQueue(process.env.RESULT_DELETE_QUEUE_NAME, { durable: true });
+        await channel.assertQueue(process.env.SOLVER_QUEUE , { durable: true });
+        await channel.assertQueue(process.env.LOG_QUEUE, { durable: true });
+        await channel.assertQueue(process.env.RESULT_DELETE_QUEUE_NAME, { durable: true });
 
     // Binding queues
-    await channel.bindQueue( process.env.SOLVER_QUEUE, solverExchange, process.env.SOLVER_ROUTING_KEY);
-    await channel.bindQueue( process.env.LOG_QUEUE, logExchange, process.env.LOG_ROUTING_KEY);
-    await channel.bindQueue( process.env.RESULT_DELETE_QUEUE_NAME, process.env.RESULT_DELETE_EXCHANGE_NAME, process.env.RESULT_DELETE_ROUTING_KEY);
+        await channel.bindQueue( process.env.SOLVER_QUEUE, solverExchange, process.env.SOLVER_ROUTING_KEY);
+        await channel.bindQueue( process.env.LOG_QUEUE, logExchange, process.env.LOG_ROUTING_KEY);
+        await channel.bindQueue( process.env.RESULT_DELETE_QUEUE_NAME, process.env.RESULT_DELETE_EXCHANGE_NAME, process.env.RESULT_DELETE_ROUTING_KEY);
 
-    channel.prefetch(1);
-    await consumeMessages(channel);
-    await consumeDeleteMessages(channel);
-    console.log('RabbitMQ correctly connected');
+        channel.prefetch(1);
+        await consumeMessages(channel);
+        await consumeDeleteMessages(channel);
 
+    } catch (error) {
+        console.error('Failed to connect or configure RabbitMQ:', error);
+        setTimeout(setupRabbitMQ, 5000);  // Retry connection after 5 seconds
+    }
 }
+
 
 async function consumeMessages(channel) {
     console.log('Listening for messages on SOLVER_QUEUE');
